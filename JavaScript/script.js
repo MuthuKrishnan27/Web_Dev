a44be231e821f2c79b484b4c538ebccea5e25523
MuthuKrishnan27 <smuthukrishnan2764@gmail.com>
2023-05-04 20:20:10 +0530

    JavaScript Basics

---
+++ JavaScript/script.js
@@ -0,0 +1,61 @@
+// alert("Alert!!!");
+// var a = prompt("a: ");
+// ans = function oddeven(a){
+//     if(a%2===0){
+//         return "even"
+//     }
+//     else{
+//         return "odd"
+//     }
+// }
+
+// console.log(ans(a)); 
+
+// hoisted();
+// function hoisted() 
+// { 
+//     console.log('Hoisted'); 
+// }
+
+// var i,j;
+// for(var i=0,j=10;i<5;++i){
+//     console.log("Hello");
+// }
+
+// var a=10;
+// function f(){
+//     var a = 20;
+// }
+// f();
+// console.log(a);
+
+
+// var a=10;
+// function f(){
+//     var a = 20;
+//     console.log(a);
+// }
+// f();
+
+// var a = 1
+// function b(){
+//     a=10;
+//     return;
+//     function a(){}
+// }
+// b();
+// console.log(a);
+
+// function mul(a,b){
+//     return a*b;
+// };
+// console.log(mul);
+
+// var a;
+// console.log(a);
+
+// function demo(){
+//     console.log(x);
+//     var x=10;
+// }
+// demo();
